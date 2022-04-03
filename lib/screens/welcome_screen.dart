eb037cad76c7a3a9d5d4efc8dd37244a344a006a
Lani <lani.mortontran@movemedical.com>
2022-04-02 17:24:17 -0700

    177. Code Refactoring Challenge

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,4 +1,5 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flash_chat/components/rounded_button.dart';
 import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
@@ -21,46 +22,17 @@ class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProvider
     controller = AnimationController(
       vsync: this,
       duration: Duration(seconds: 1),
-
-      // upperBound: 100, // when a curve is applied, upperBound cannot be greater than 1 since they draw from 0 to 1.
     );
-
-    // parent: what animation we want to apply this curve to
-    // curve: what type of curve
-    /*
-    animation = CurvedAnimation(
-      parent: controller,
-      curve: Curves.decelerate,
-    );
-    */
 
     animation = ColorTween(
-      begin: Colors.white,
-      end: Colors.blueGrey,
+      begin: Colors.blueGrey,
+      end: Colors.white,
     ).animate(controller);
 
     controller.forward();
-    // controller.reverse(from: 1.0);
-
-    /*
-    animation.addStatusListener((status) {
-      // forward() = Animation.completed
-      // reverse() = Animation.dismissed
-      print(status);
-
-      // This allows for an endless loop
-      if (status == AnimationStatus.completed) {
-        controller.reverse(from: 1.0);
-      } else if (status == AnimationStatus.dismissed) {
-        controller.forward();
-      }
-    });
-    */
 
     controller.addListener(() {
       setState(() {});
-      // print(controller.value);
-      print(animation.value);
     });
   }
 
@@ -86,7 +58,6 @@ class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProvider
                   tag: 'logo',
                   child: Container(
                     child: Image.asset('images/logo.png'),
-                    // height: animation.value * 100, // multiply by 100 since value ranges from 0 to 1
                     height: 60.0,
                   ),
                 ),
@@ -101,55 +72,27 @@ class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProvider
                     ),
                   ],
                 ),
-                // Text(
-                //   'Flash Chat',
-                //   style: TextStyle(
-                //     fontSize: 45.0,
-                //     fontWeight: FontWeight.w900,
-                //   ),
-                // ),
               ],
             ),
             SizedBox(
               height: 48.0,
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                elevation: 5.0,
-                color: Colors.lightBlueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                child: MaterialButton(
-                  onPressed: () {
-                    //Go to login screen.
-                    Navigator.pushNamed(context, LoginScreen.id);
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Log In',
-                  ),
-                ),
-              ),
+            RoundedButton(
+              color: Colors.lightBlueAccent,
+              title: 'Log In',
+              onPressed: () {
+                //Go to login screen.
+                // print('going to login screen');
+                Navigator.pushNamed(context, LoginScreen.id);
+              },
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                color: Colors.blueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () {
-                    //Go to registration screen.
-                    Navigator.pushNamed(context, RegistrationScreen.id);
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Register',
-                  ),
-                ),
-              ),
+            RoundedButton(
+              color: Colors.blueAccent,
+              title: 'Register',
+              onPressed: () {
+                //Go to registration screen.
+                Navigator.pushNamed(context, RegistrationScreen.id);
+              },
             ),
           ],
         ),
